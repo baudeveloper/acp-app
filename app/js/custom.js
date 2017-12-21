5d1f6c5dce969bc3ec10e60e642a205cc8d1fbff
Baudeveloper <webmaster@baustein.ca>
2017-12-21 09:05:44 -0500

    Revised bootstrap vars in Sass file

--- app/js/custom.js
+++ app/js/custom.js
@@ -3,7 +3,7 @@
 //========================
 "use strict";
 
-$(function() {
+$(function () {
 
   /*
     1. SMOOTH SCROLLING: https://tinyurl.com/l4voz9b [UNIVERSAL]
@@ -15,37 +15,31 @@ $(function() {
   */
 
   // 1. SMOOTH SCROLLING: https://tinyurl.com/l4voz9b [UNIVERSAL]
-	$("a[href*=\"#\"]")
-    .not("[href=\"#\"]")
-    .not("[href=\"#0\"]")
-    .click(function (event) {
-	if (
-      location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-		var target = $(this.hash);
-		target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-		if (target.length) {
-			event.preventDefault();
-			$("html, body").animate({
-				scrollTop: target.offset().top
-			}, 1000, function () {
-				var $target = $(target);
-				$target.focus();
-				if ($target.is(":focus")) {
-					return false;
-				} else {
-					$target.attr("tabindex", "-1");
+	$("a[href*=\"#\"]").not("[href=\"#\"]").not("[href=\"#0\"]").click(function (event) {
+		if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+			if (target.length) {
+				event.preventDefault();
+				$("html, body").animate({
+					scrollTop: target.offset().top
+				}, 1000, function () {
+					var $target = $(target);
 					$target.focus();
-				}
-			});
+					if ($target.is(":focus")) {
+						return false;
+					} else {
+						$target.attr("tabindex", "-1");
+						$target.focus();
+					}
+				});
+			}
 		}
-	}
-});
+	});
 
   // 2. SEARCH IN NAVIGATION [UNIVERSAL]
 	var $searchBtn = $("[data-js=\"searchBtn\"]");
-	$searchBtn.on("click", function() {
+	$searchBtn.on("click", function () {
 		var $getSearchBox = $(this).parents(".input-group");
 		if ($getSearchBox.hasClass("hide")) {
 			$getSearchBox.removeClass("hide");
